5f1c1df6e4b7dc9f31e5f4974f17e906b792e296
sataandagii <nikagoncharenko0@gmail.com>
2025-03-27 00:24:29 +0200

    fourth(!) query

--- Lab1/Lab1/DataQueries.cs
+++ Lab1/Lab1/DataQueries.cs
@@ -67,28 +67,59 @@ namespace Lab1
 
         public void Find6OrMoreUniqueDonations()
         {
-            //var query3 = from donor in Data.Donors
-            //         join report in Data.Reports on donor.DonorId equals report.DonorId
-            //         group report by donor.DonorName into groupedReports
-            //         where groupedReports.Select(grouped => grouped.OrganisationId).Distinct().Count() > 5
-            //         &&
-            //         groupedReports.Count() == groupedReports.Select(grouped => grouped.OrganisationId).Distinct().Count()
-            //         select groupedReports.Key;
+            var query3 = from donor in Data.Donors
+                         join report in Data.Reports on donor.DonorId equals report.DonorId
+                         group report by donor.DonorName into groupedReports
+                         where groupedReports.Select(grouped => grouped.OrganisationId).Distinct().Count() > 5
+                         &&
+                         groupedReports.Count() == groupedReports.Select(grouped => grouped.OrganisationId).Distinct().Count()
+                         select groupedReports.Key;
 
-            var query3 = Data.Donors.Join(Data.Reports,
-                                    donor => donor.DonorId,
-                                    report => report.DonorId,
-                                    (donor, report) => new { donor, report })
-                                    .GroupBy(joined => joined.donor.DonorName)
-                                    .Where(grouped => grouped.Select(joined => joined.report.OrganisationId).Distinct().Count() > 5
-                                    &&
-                                    grouped.Select(joined => joined.report.OrganisationId).Distinct().Count() == grouped.Count())
-                                    .Select(g => g.Key);
+            //var query3 = Data.Donors.Join(Data.Reports,
+            //                        donor => donor.DonorId,
+            //                        report => report.DonorId,
+            //                        (donor, report) => new { donor, report })
+            //                        .GroupBy(joined => joined.donor.DonorName)
+            //                        .Where(grouped => grouped.Select(joined => joined.report.OrganisationId).Distinct().Count() > 5
+            //                        &&
+            //                        grouped.Select(joined => joined.report.OrganisationId).Distinct().Count() == grouped.Count())
+            //                        .Select(g => g.Key);
 
             Console.WriteLine("3. Визначити донорів, які зробили внески у більше ніж 5 фондів, але не більше одного разу в кожен фонд.");
 
             foreach (var name in query3)
                 Console.WriteLine($"\t{name}");
+        }
+
+        public void DonorsWith3OrganisationsWith2Projects()
+        {
+            var organisationsWith2Projects = from project in Data.Projects
+                                             group project by project.OrganisationId into groupedProjects
+                                             where groupedProjects.Count() >= 2
+                                             select groupedProjects.Key;
+
+            var validReports = from report in Data.Reports
+                               where organisationsWith2Projects.Contains(report.OrganisationId)
+                               select new { report.DonorId, report.OrganisationId, report.SpentMoney, report.RecievedMoney };
+
+            var donorsWith3Organisations = from report in validReports
+                                           group report by report.DonorId into groupedReports
+                                           where groupedReports.Select(r => r.OrganisationId).Distinct().Count() >= 3
+                                           select groupedReports.Key;
+
+            var query4 = from report in validReports
+                         where donorsWith3Organisations.Contains(report.DonorId)
+                         group report by report.DonorId into groupedReports
+                         select new
+                         {
+                             DonorName = Data.Donors.First(d => d.DonorId == groupedReports.Key).DonorName,
+                             Average = groupedReports.Average(rep => (rep.SpentMoney / rep.RecievedMoney) * 200)
+                         };
+
+            Console.WriteLine("4. Знайти донорів, які фінансували принаймні три різні організації, де кожна організація реалізувала хоча б два проєкти, і визначити\r\nсередній відсоток витрат на адміністративні потреби у цих проєктах");
+
+            foreach (var name in query4)
+                Console.WriteLine($"\tДонор: {name.DonorName}, Відсоток витрат: {name.Average}%");
 
         }
 
--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -14,6 +14,7 @@
             query.DonatedTo3Organisations();
             query.Last3Months();
             query.Find6OrMoreUniqueDonations();
+            query.DonorsWith3OrganisationsWith2Projects();
         }
 
     }
